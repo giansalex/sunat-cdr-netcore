b8595771cdea5e3986d2c5588aef2e0b54c277ea
Giancarlos Salas <giansalex@gmail.com>
2019-05-22 20:33:49 -0500

    Agregar archivos de proyecto.

---
+++ Sunat.Cdr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sunat.Cdr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
